3465276a91be14e6f2a866cd1d36a560442b5d4b
akshar <glaivesharp@gmail.com>
2024-11-06 14:10:17 +0530

    Add page visit api endpoint to site config

--- src/site.config.ts
+++ src/site.config.ts
@@ -27,6 +27,7 @@ export const siteConfig: SiteConfig = {
 		// Webmention.io API endpoint. Get your own here: https://webmention.io/, and follow this blog post: https://astro-cactus.chriswilliams.dev/posts/webmentions/
 		link: "",
 	},
+  pageVisitAPI: "<enter your new api endpoint link here>"
 };
 
 // Used to generate links in both the Header & Footer.
